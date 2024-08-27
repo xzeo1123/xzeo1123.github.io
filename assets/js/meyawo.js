05743e2d435078c9ee6cbd511cb6d0beeb569851
xzeo1123 <xzeo1123@gmail.com>
2024-08-27 21:15:38 +0700

    updating

--- assets/js/meyawo.js
+++ assets/js/meyawo.js
@@ -22,3 +22,19 @@ $('#nav-toggle').click(function(){
     $(this).toggleClass('is-active')
     $('ul.nav').toggleClass('show');
 });
+
+$(document).ready(function(){
+    const projectLinks = document.querySelectorAll('.project-link');
+
+    projectLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+
+            const projectUrl = this.getAttribute('href');
+
+            alert("Details Project page is working on so have a view at my github instead while we are working on!");
+
+            window.open(projectUrl, '_blank');
+        });
+    });
+});
